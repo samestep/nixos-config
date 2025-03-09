be912a9210429e617bc1d5f37c0fbb669d5f27b0
Sam Estep <sam@samestep.com>
2025-03-09 17:24:45 -0400

    Install Steam

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -93,6 +93,14 @@
   # Install firefox.
   programs.firefox.enable = true;
 
+  # https://nixos.wiki/wiki/Steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
